9ef97539b3d2089dcd2f2232a0f937fff3dc543f
Igna <cuknoris@gmail.com>
2016-09-22 16:16:54 +0700

    v1

---
+++ README.md
@@ -0,0 +1,7 @@
+## Library Management
+
+App for managing Article, Members, Memberships and Transactionns for Libraries
+
+#### License
+
+GNU General Public License
---
+++ library_management/config/desktop.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from frappe import _
+
+def get_data():
+	return [
+		{
+			"module_name": "Library Management",
+			"color": "#9400D3",
+			"icon": "octicon octicon-device-camera",
+			"type": "module",
+			"label": _("Library Management")
+		}
+	]
